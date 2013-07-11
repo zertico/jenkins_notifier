a3def3922c78aa4f031ffe8772c5d91eefb1f3d7
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2013-07-11 17:39:17 -0300

    Initial Commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,3 @@
+class ApplicationController < Zertico::Controller
+  protect_from_forgery
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JenkinsNotifier::Application.config.secret_token = '45bab69fddaf3bbe9e894d94b2399ced3be3bad5ad826d413216abfaebfe5cc47e7d4ac1227cd4dbe725c9acdd351496580279b27fcea038a45e1a8d99d04583'
