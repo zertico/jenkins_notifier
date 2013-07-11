d9ed5883cfe151cbd70e029ffea3b6d0fe97f755
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2013-07-11 18:39:26 -0300

    Validating Project

--- app/interfaces/project.rb
+++ app/interfaces/project.rb
@@ -1,3 +1,8 @@
 class Project < ActiveRecord::Base
   attr_accessible :bitbucket, :endpoint, :name, :token
+
+  validates :bitbucket, :presence => true
+  validates :endpoint, :presence => true
+  validates :name, :presence => true
+  validates :token, :presence => true
 end
