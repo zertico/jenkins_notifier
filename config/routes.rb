8b20adf8c693e4bd78f16d49e80cdc8d51834175
Paulo Henrique Lopes Ribeiro <plribeiro3000@gmail.com>
2013-07-23 15:20:41 -0300

    Addin Sidekiq web

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
+require 'sidekiq/web'
+
 JenkinsNotifier::Application.routes.draw do
   resources :projects do
     resources :notifications, :only => [ :create ]
     resources :jobs, :only => [ :create ]
   end
+
+  mount Sidekiq::Web => '/sidekiq'
 
   root :to => 'projects#index'
 end
